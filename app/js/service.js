e60dc5d627c5f287629360dcc778a15e1594357e
sivakavi <sivakavi63@gmail.com>
2016-04-17 12:50:58 +0530

    Serivce file added for recent activites

---
+++ app/js/service.js
@@ -0,0 +1,9 @@
+// Service list
+
+// Get service for recent activities 
+$http.get("welcome.htm")
+    .then(function(response) {
+        $scope.myWelcome = response.data;
+    }, function(error){
+        
+    });
